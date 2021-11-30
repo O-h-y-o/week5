8fa8421ae8f75ad327d44e87afd5ca17c82f377e
김형래 <khr157929@gmail.com>
2021-11-30 10:36:34 +0900

    로그인 시 보여줄 화면

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,23 @@
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Btn from "../elements/btn";
+import { getCookie, setCookie } from "../shared/cookies";
 import BtnLogin from "../elements/btn_signIn";
 
-export default function main() {
-  let id;
-  if (id === null) {
+export default function Main() {
+  const [is_login, setIsLogin] = React.useState(false);
+
+  React.useEffect(() => {
+    let cookie = getCookie();
+    console.log(cookie);
+    if (cookie) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, []);
+
+  if (is_login === null) {
     return (
       <>
         <BtnLogin />
